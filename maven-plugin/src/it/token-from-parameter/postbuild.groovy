a53702960af82902cd85870299c89bd45059c08f
Mikaël Barbero <mikael.barbero@eclipse-foundation.org>
2026-03-05 00:10:23 +0100

    test: remove token-from-parameter IT (apiToken param removed)

--- maven-plugin/src/it/token-from-parameter/postbuild.groovy
+++
@@ -1,3 +0,0 @@
-File signedFile = new File(basedir, 'target/classes/app.exe')
-assert signedFile.exists(), "Signed file does not exist: ${signedFile}"
-assert signedFile.text == 'signed-content', "Expected 'signed-content' but got: '${signedFile.text}'"
